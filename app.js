97517cebd843c30decc51f866d3e32aaa2d11b8c
Jason Paredes <haloos@me.com>
2019-01-04 19:53:17 -0500

    Display the weather

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
-
+// Init UI
+const ui = new UI();
 weather.getWeather()
 .then(results => {
-  console.log(results);
+ ui.paint(results);
 })
 .catch(err => console.log(err));
